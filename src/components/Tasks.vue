c11a2969b072bc4d721b5ea59cbefec40472271d
Rakesh Rankawat <rakesh.rankawat@outlook.com>
2022-12-30 17:37:47 +0530

    Tasks

---
+++ src/components/Tasks.vue
@@ -0,0 +1,17 @@
+<template>
+  <div v-for="task in tasks" :key="task.id">
+    <Task :task="task" />
+  </div>
+</template>
+
+<script>
+import Task from './Task'
+
+export default {
+  name: 'Tasks',
+  props: {
+    tasks: Array,
+  },
+  components: { Task },
+}
+</script>
