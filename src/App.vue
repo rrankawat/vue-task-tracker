e7036a34ede4f0b24414c81186cb2ced63c6f64f
Rakesh Rankawat <rakesh.rankawat@outlook.com>
2023-01-05 23:17:01 +0530

    json-server & proxy

--- src/App.vue
+++ src/App.vue
@@ -46,28 +46,15 @@ export default {
         task.id === id ? { ...task, reminder: !task.reminder } : task
       )
     },
+    async fetchTasks() {
+      const res = await fetch('api/tasks')
+      const data = await res.json()
+
+      return data
+    },
   },
-  created() {
-    this.tasks = [
-      {
-        id: 1,
-        text: 'Doctors Appointment',
-        day: 'March 1st at 2:30pm',
-        reminder: true,
-      },
-      {
-        id: 2,
-        text: 'Meeting at School',
-        day: 'March 3rd at 1:30pm',
-        reminder: true,
-      },
-      {
-        id: 3,
-        text: 'Food Shopping',
-        day: 'March 5th at 11:00am',
-        reminder: false,
-      },
-    ]
+  async created() {
+    this.tasks = await this.fetchTasks()
   },
 }
 </script>
