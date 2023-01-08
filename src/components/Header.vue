ccb534f02ab02f25b5940f087ca45a9f5d8d29fa
Rakesh Rankawat <rakesh.rankawat@outlook.com>
2023-01-08 13:01:03 +0530

    Vue Router

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,6 +2,7 @@
   <header>
     <h1>{{ title }}</h1>
     <Button
+      v-show="homePage"
       @btn-click="$emit('toggle-add-task')"
       :text="showAddTask ? 'Close' : 'Add Task'"
       :color="showAddTask ? 'red' : 'green'"
@@ -18,7 +19,18 @@ export default {
     title: String,
     showAddTask: Boolean,
   },
-  components: { Button },
+  components: {
+    Button,
+  },
+  computed: {
+    homePage() {
+      if (this.$route.path === '/') {
+        return true
+      } else {
+        return false
+      }
+    },
+  },
 }
 </script>
 
